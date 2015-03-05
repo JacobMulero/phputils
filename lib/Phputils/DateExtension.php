4b52db059bff54a10a8c89421252e2e20993d398
Jaime Mulero Molinet <jmulero@lunia.es>
2015-03-05 10:21:50 +0100

    Modificaciones

--- lib/Phputils/DateExtension.php
+++ lib/Phputils/DateExtension.php
@@ -7,6 +7,43 @@
 
 namespace Phputils;
 
-class DateExtension {
+use DateTime;
+use DateInterval;
+
+class DateExtension extends DateTime{
+
+    /**
+     * Return Date in ISO8601 format
+     *
+     * @return String
+     */
+    public function __toString() {
+        return $this->format('Y-m-d');
+    }
+
+    /**
+     * Return difference between $this and $now
+     *
+     * @param Datetime|String $now
+     * @param bool $absolute
+     * @return DateInterval
+     */
+    public function diff($now = 'NOW',$absolute = false) {
+        if(!($now instanceOf DateTime)) {
+            $now = new DateTime($now);
+        }
+        return parent::diff($now);
+    }
+
+    /**
+     * Return Age in Years
+     *
+     * @param Datetime|String $now
+     * @return Integer
+     */
+    public function getAge($now = 'NOW') {
+        return $this->diff($now)->format('%y');
+    }
+
 
 }
